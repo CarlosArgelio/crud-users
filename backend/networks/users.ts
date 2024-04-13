d9dec94c7845667a1512120e3008896a9f0bb801
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-12 23:44:10 -0400

    feat: func succes to endpoints

--- backend/networks/users.ts
+++ backend/networks/users.ts
@@ -1,5 +1,6 @@
-import e, { Router, Request, Response, NextFunction } from "express";
+import { Router, Request, Response, NextFunction } from "express";
 import { UserController } from "./../controllers";
+import { success } from "./../middlewares/response";
 
 const users = Router();
 
@@ -7,7 +8,7 @@ const findAll = (req: Request, res: Response, next: NextFunction) => {
     try {
         const controller = new UserController()
         const users = controller.findAll()
-        res.json(users)
+        success(req, res, users, 200)
     } catch (error) {
         next(error)
     }
@@ -18,7 +19,7 @@ const findOne = (req: Request, res: Response, next: NextFunction) => {
     try {
         const controller = new UserController()
         const user = controller.findOne(id)
-        res.json(user)
+        success(req, res, user, 200)
     } catch (error) {
         next(error)
     }
@@ -29,7 +30,7 @@ const create = (req: Request, res: Response, next: NextFunction) => {
     try {
         const controller = new UserController()
         const user = controller.create(body)
-        res.json(user)   
+        success(req, res, user, 201)
     } catch (error) {
         next(error)
     }
@@ -42,7 +43,7 @@ const update = (req: Request, res: Response, next: NextFunction) => {
     try {
         const controller = new UserController()
         const user = controller.update(id, changes)
-        res.json(user)
+        success(req, res, user, 200)
     } catch (error) {
         next(error)
     }
@@ -54,7 +55,7 @@ const remove = (req: Request, res: Response, next: NextFunction) => {
     try {
         const controller = new UserController()
         const user = controller.delete(id)
-        res.json(user)   
+        res.status(204)
     } catch (error) {
         next(error)
     }
