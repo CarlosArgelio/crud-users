99ffd1c36333c5faf62ae4e237b35b62ec70af5d
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-12 23:41:42 -0400

    feat: add trycath on netwoks

--- backend/networks/users.ts
+++ backend/networks/users.ts
@@ -1,44 +1,63 @@
-import { Router, Request, Response, NextFunction } from "express";
+import e, { Router, Request, Response, NextFunction } from "express";
 import { UserController } from "./../controllers";
 
 const users = Router();
 
 const findAll = (req: Request, res: Response, next: NextFunction) => {
-    const controller = new UserController()
-    const users = controller.findAll()
-    res.json(users)
+    try {
+        const controller = new UserController()
+        const users = controller.findAll()
+        res.json(users)
+    } catch (error) {
+        next(error)
+    }
 }
 
 const findOne = (req: Request, res: Response, next: NextFunction) => {
     const { id } = req.params 
-
-    const controller = new UserController()
-    const user = controller.findOne(id)
-    res.json(user)
+    try {
+        const controller = new UserController()
+        const user = controller.findOne(id)
+        res.json(user)
+    } catch (error) {
+        next(error)
+    }
 }
 
 const create = (req: Request, res: Response, next: NextFunction) => {
     const { body } = req
-    const controller = new UserController()
-    const user = controller.create(body)
-    res.json(user)
+    try {
+        const controller = new UserController()
+        const user = controller.create(body)
+        res.json(user)   
+    } catch (error) {
+        next(error)
+    }
 }
 
 const update = (req: Request, res: Response, next: NextFunction) => {
     const { id } = req.params
     const changes = req.body
 
-    const controller = new UserController()
-    const user = controller.update(id, changes)
-    res.json(user)
+    try {
+        const controller = new UserController()
+        const user = controller.update(id, changes)
+        res.json(user)
+    } catch (error) {
+        next(error)
+    }
 }
 
 const remove = (req: Request, res: Response, next: NextFunction) => {
     const { id } = req.params
 
-    const controller = new UserController()
-    const user = controller.delete(id)
-    res.json(user)
+    try {
+        const controller = new UserController()
+        const user = controller.delete(id)
+        res.json(user)   
+    } catch (error) {
+        next(error)
+    }
 }
 
 users.get("/", findAll)
