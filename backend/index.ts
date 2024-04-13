009b85fb0e3d232b92f61984447a71d25dc271b2
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-12 23:19:58 -0400

    feat: configuration middlewares in index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -3,7 +3,10 @@ import dotenv from 'dotenv'
 
 import BodyParser from "body-parser";
 
+import middlewares from "./middlewares";
 import { networks } from "./networks";
+
+const { logErrors, errorHandler, boomErrorHandler } = middlewares;
 
 dotenv.config({
     path: './../.env'
@@ -21,6 +24,10 @@ app.use(BodyParser.json());
 
 // Endpoints
 networks(app);
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
